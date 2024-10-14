679d255c4439e9f2d933f2a74759fbd50bbd2753
mjallen729 <lumpychum@gmail.com>
2024-10-13 20:13:19 -0400

    added callback to notesInput comp

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import NotesInput from './components/InputNotes';
 import './App.css';
 
 function App() {
+  const [found, setFound] = useState([]);
 
   return (
     <div id='root'>
       <div style={{ padding: '20px' }}>
         <h1>Musical Notes Input</h1>
-        <NotesInput />
+        <NotesInput onSubmit={setFound}/>
       </div>
     </div>
   )
